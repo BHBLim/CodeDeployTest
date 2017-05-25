77ae11e5f2ee7b4cdd33b8c32018e915cb7741da
Basil Lim <BHB_Lim@yahoo.com>
2017-05-25 15:36:46 +0800

    Added date stuff

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,5 @@
 cd /home/ubuntu/CodeDeployTestApp
 touch testfile
 forever start -c http-server
+echo "Started node server at $(date) " > codeDeploy.log
 exit 0
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,4 +2,5 @@
 isExistApp = `pgrep node`
 if [[ -n  $isExistApp ]]; then
     kill isExistApp
+    echo "Killed node server at $(date) " > codeDeploy.log
 fi
