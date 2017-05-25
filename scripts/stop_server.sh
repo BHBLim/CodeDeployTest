fcb4929950e7573fec375d106400d588340da347
Basil Lim <BHB_Lim@yahoo.com>
2017-05-25 16:13:12 +0800

    Fixed variable assignment

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu/CodeDeployTestApp
-isExistApp = `pgrep node`
+isExistApp=`pgrep node`
 echo $isExistApp >> codeDeploy.log
 if [[ -n  $isExistApp ]]; then
-    kill $isExistApp
+    kill "$isExistApp""
     echo "Killed node server at $(date) " >> codeDeploy.log
 fi
