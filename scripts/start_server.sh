4e95986d889e3b2caca2c676a9deffb6e016773d
Basil Lim <BHB_Lim@yahoo.com>
2017-05-25 16:26:13 +0800

    Should be working now

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/CodeDeployTestApp
 touch testfile
-http-server --cors -p 12629 &
+http-server --cors -p 12629 . &
 echo "Started node server at $(date) " >> codeDeploy.log
 exit 0
